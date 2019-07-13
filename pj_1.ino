95fee85aff0b4857605cf9351e87301578ce3163
EaintMyatThu <52852290+EaintMyatThu@users.noreply.github.com>
2019-07-13 15:46:02 +0630

    first

---
+++ pj_1.ino
@@ -0,0 +1,16 @@
+int pinState;
+void setup() {
+  pinMode(10,INPUT);
+  Serial.begin(9600);
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  pinState=digitalRead(10);
+  Serial.println(pinState);
+  delay(100);
+  
+  // put your main code here, to run repeatedly:
+
+}
